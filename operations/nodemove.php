b8c62239992a3de8c30ddd56743585cea70921ba
Arne <bratt@nvg.ntnu.no>
2012-04-11 13:38:12 +0200

    Added nodemove option to be able to move node up in the content tree relative to current position.

--- operations/nodemove.php
+++ operations/nodemove.php
@@ -6,35 +6,54 @@ class nodemoveOperation
     function getHelpText()
     {
         return '
---operation="nodemove;target=<node id>"
+--operation="nodemove;target=<node id>;move_to_depth=<depth>"
 
 target - Node ID of the target node to move selected nodes under
+move_to_depth - Depth of content tree to move the node up to. Must always be above current depth!
 ';
     }
 
     function setParameters( $parm_array )
     {
         // Make sure no unsupported parameters are specified
-        $supported_parameters = array( 'target' );
+        $supported_parameters = array( 'target', 'move_to_depth' );
         $parm_keys = array_keys( $parm_array );
         $unsupported_list = array_diff( $parm_keys, $supported_parameters );
         if ( isset( $unsupported_list[0] ) )
             return "Unsupported parameter '{$unsupported_list[0]}' in operation";
 
         $this->target_id = intval( $parm_array[ 'target' ] );
-        if ( $this->target_id == 0 )
-            return 'Missing or illegal target node id';
+        $this->move_to_depth = intval( $parm_array[ 'move_to_depth' ] );
+        if ( $this->target_id == 0 AND $this->move_to_depth == 0 )
+            return 'Missing target node id and target depth to move to';
+        if ( $this->target_id > 0 AND $this->move_to_depth > 0 )
+            return 'Target node id and target depth can not be used simultaniously';
         return true;
     }
 
     // Move the given node to the specified target
     // target_id - Node id of the node to move to
+    // move_to_depth - Depth of content tree to move up to
     function runOperation( &$object )
     {
-        return moveNode( $object, $this->target_id );
+        $target_id = $this->target_id;
+
+        if ( empty( $target_id ) )
+        {
+            if ( $this->move_to_depth >= $object->attribute( 'depth' ) )
+            {
+                return false;
+            }
+            // Find the correct target node for the specified depth
+            $path_array = $object->attribute( 'path_array' );
+            $target_id = $path_array[$this->move_to_depth - 1];
+        }
+
+        return moveNode( $object, $target_id );
     }
 
     var $target_id;
+    var $move_to_depth;
 }
 
 ?>
